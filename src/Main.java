b83478b9eedc9d1efcbc41a7293621cf8cf8aea4
chptr-one <59266782+chptr-one@users.noreply.github.com>
2020-05-17 16:45:47 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String expr = "(2 * 3 - (3 - 1) * 2) / 0.5";
-        Parser parser = new Parser(expr);
-        System.out.println(expr + " = " + parser.result());
-    }
-}
